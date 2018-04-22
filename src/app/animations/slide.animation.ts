244a0d2ac463e11396c7ee9d8bc400c96aa04b2a
ksab <arabaske@gmail.com>
2018-04-22 21:08:57 +0200

    animation in-out

--- src/app/animations/slide.animation.ts
+++ src/app/animations/slide.animation.ts
@@ -3,18 +3,18 @@ import { trigger, animate, transition, style, keyframes, animation} from '@angul
 export const SlideAnim = trigger('slideState', [
     transition('right-sided => active', [
       style({transform: 'translateX(0)'}),
-      animate('1000ms', style({transform: 'translateX({{slide_pct_left}})'}))
+      animate('1000ms ease-in-out', style({transform: 'translateX({{slide_pct_left}})'}))
     ], {params: {slide_pct_left: "-25%"}}),
     transition('active => left-sided', [
       style({transform: 'translateX(0)'}),
-      animate('1000ms', style({transform: 'translateX({{slide_pct_left}})'}))
+      animate('1000ms ease-in-out', style({transform: 'translateX({{slide_pct_left}})'}))
     ], {params: {slide_pct_left: "-25%"}}),
     transition('left-sided => active', [
       style({transform: 'translateX({{slide_pct_left}})'}),
-      animate('1000ms', style({transform: 'translateX(0)'}))
+      animate('1000ms ease-in-out', style({transform: 'translateX(0)'}))
     ], {params: {slide_pct_left: "-25%"}}),
     transition('active => right-sided', [
       style({transform: 'translateX({{slide_pct_left}})'}),
-      animate('1000ms', style({transform: 'translateX(0)'}))
+      animate('1000ms ease-in-out', style({transform: 'translateX(0)'}))
     ], {params: {slide_pct_left: "-25%"}}),
   ])
